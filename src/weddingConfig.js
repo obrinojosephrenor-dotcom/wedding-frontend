9307209fc531d05c3995cb21313aabb6a00f3b0a
Joseph Renor Obrino <obrino.josephrenor@nwssu.edu.ph>
2026-06-10 18:19:55 +0800

    Fix the map lat and lng for accuracy

--- src/weddingConfig.js
+++ src/weddingConfig.js
@@ -9,8 +9,8 @@
     venue: "Centenial Pastoral Center",
     address: "Brgy. West Awang, Calbayog City",
     time: "3:00 PM",
-    lat: 12.0678,
-    lng: 124.5935,
+    lat: 12.066165910498562,
+    lng: 124.59601319799276,
     mapLink: "https://maps.app.goo.gl/8GUWZY8FTFCvd6Fk8",
   },
 
@@ -18,8 +18,8 @@
     venue: "Buenaventura Suites",
     address: "Brgy. Carmen, Calbayog City",
     time: "5:00 PM",
-    lat: 12.0645,
-    lng: 124.5935,
+    lat: 12.069339624130825,
+    lng: 124.58843533084563,
     mapLink: "https://maps.app.goo.gl/nFGwkGJfEcwnPtPU8",
   },
 
