8ad3cd85b760951297f561f738cb923188631286
Joseph Renor Obrino <obrino.josephrenor@nwssu.edu.ph>
2026-06-10 18:08:54 +0800

    Fix map link accuracy

--- src/weddingConfig.js
+++ src/weddingConfig.js
@@ -11,7 +11,7 @@
     time: "3:00 PM",
     lat: 12.0647,
     lng: 124.6054,
-    mapLink: "https://maps.app.goo.gl/f8stfHquw5UcaB127?g_st=afm",
+    mapLink: "https://maps.app.goo.gl/8GUWZY8FTFCvd6Fk8",
   },
 
   reception: {
@@ -20,7 +20,7 @@
     time: "5:00 PM",
     lat: 12.0663,
     lng: 124.6072,
-    mapLink: "https://maps.app.goo.gl/yuXgymnEbFeXjgqSA?g_st=afm",
+    mapLink: "https://maps.app.goo.gl/nFGwkGJfEcwnPtPU8",
   },
 
   dresscode: "Garden Formal",
